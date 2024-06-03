bd4d730aaf51a39d5e8a70003559974a41898857
Uttam Pradhan <uttampra1@gmail.com>
2024-06-03 22:16:38 +0545

    aligned the item vertically

--- src/app/components/menu.tsx
+++ src/app/components/menu.tsx
@@ -6,24 +6,24 @@ const menu = () => {
     const [open, setOpen] = useState(false)
   return (
     <div>
-        <img src="./menu.png" alt='not Supported'
-            width={28}
-            height={28}
-            onClick={() => setOpen((prev)=> !prev)}
-        />{
-            open && 
-            <div className=''>
-                <Link href="/">Home</Link>
-                <Link href="/">Home</Link>
-                <Link href="/">Home</Link>
-                <Link href="/">Home</Link>
-                <Link href="/">Home</Link>
-                <Link href="/">Home</Link>
-            </div>
-        }
-
+      <img
+      className='cursor-pointer'
+        src="./menu.png"
+        width={28}
+        height={28}
+        alt="Browser Not Supported"
+        onClick={() => setOpen((prev) => !prev)}
+      />
+      {open && (
+        <div className='flex flex-col text-red-700'>
+          <Link href="/">Home</Link>
+          <Link href="/">Home</Link>
+          <Link href="/">Home</Link>
+          <Link href="/">Home</Link>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default menu
